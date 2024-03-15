d81afb182a8578a92cd3fd6da2a1db36f4c422e4
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2024-03-15 18:30:12 -0500

    Creación Primer Codigo
    Descripcion opcional

---
+++ Primer_Codigo/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Rivar Drone Labs                                             #
+# 	Created:      3/15/2024, 6:22:11 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
